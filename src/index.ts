c4c335404c65f190956aa79007db9ae42ad0f692
Dipan <dipanmaulick25@gmail.com>
2026-01-09 07:20:03 +0000

    route skeleton

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+const app = express();
+
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+app.post("/api/v1/signup" , (req , res)=>{
+
+})
+
+app.post("/api/v1/login" , ( req ,res) =>{
+
+})
+
+app.post("/api/v1/content" , (req ,res)=>{
+
+})
+
+app.get("/api/v1/content" , ( req ,res)=>{
+
+})
+
+app.delete("/api/v1/content" , (req ,res)=>{
+
+})
+
+app.post("/api/v1/braindb/share" , (req ,res)=>{
+
+})
+
+app.get("/api/v1/braindb/:sharedLink" , (req ,res)=>{
+
+})
+
+app.listen(3000);
+
+
+
+
+
+
+
